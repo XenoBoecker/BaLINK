336c4a29051e719b0cf6be7636efb41e381725c5
XenoBoecker <xeno.boecker@web.de>
2025-06-20 13:26:27 +0200

    only once interactable for door button

--- Assets/3_Interactables/DoorButton.cs
+++ Assets/3_Interactables/DoorButton.cs
@@ -4,12 +4,19 @@ public class DoorButton : Interactable
 {
     [SerializeField] private GameObject door; // Reference to the door GameObject
 
+    [SerializeField] private bool onlyInteractableOnce;
+
     public override void Interact()
     {
         base.Interact();
 
         if (door != null)
         {
+            if(onlyInteractableOnce && HasBeenInteractedWithThisGame)
+            {
+                return; // Exit if the button can only be interacted with once
+            }
+
             // Toggle the door's active state when the button is pressed
             door.SetActive(!door.activeSelf);
         }
