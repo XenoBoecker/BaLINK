d6868d492c3357fa1d46fa1502e0b8a308451186
BouncyPantaloons <71738150+BouncyPantaloons@users.noreply.github.com>
2025-06-28 02:06:30 +0200

    Added new effects and made graph prefab

---
+++ Assets/20_Graph System/Runtime/Node Elements/Effects/DropItem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[NodeElement(NodeType.Effect)]
+[RequireComponent(typeof(EquippedItem))]
+public class DropItem : Effect
+{
+    public override void TriggerEffect()
+    {
+        Destroy(ReferencedObject);
+    }
+    
+    public override string GetName()
+    {
+        return "Drop Item";
+    }
+}
