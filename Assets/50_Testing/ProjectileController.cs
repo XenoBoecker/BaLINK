92a5ea19fb5c51c00cba30ff8376de04287ae109
XenoBoecker <xeno.boecker@web.de>
2025-07-02 18:07:22 +0200

    new crossbow and new target

--- Assets/50_Testing/ProjectileController.cs
+++ Assets/50_Testing/ProjectileController.cs
@@ -15,6 +15,33 @@ public class ProjectileController : MonoBehaviour
     {
         _rb.isKinematic = true;
     }
+    private void OnTriggerEnter(Collider collider)
+    {
+        Hitable hitable = collider.gameObject.GetComponent<Hitable>();
+
+        if (hitable == null)
+        {
+            return;
+        }
+
+        hitable.Hit(transform);
+
+        if (hitable.ShootThroughObject)
+        {
+            return;
+        }
+
+        transform.parent = collider.transform;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true; // Make the Rigidbody kinematic to stop further movement
+            rb.linearVelocity = Vector3.zero; // Stop the bolt's movement
+            rb.angularVelocity = Vector3.zero;
+            rb.useGravity = false; // Disable gravity to prevent falling
+        }
+    }
 
     internal void Shoot(float shootForce)
     {
