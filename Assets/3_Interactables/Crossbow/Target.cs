390ece4028e495488bcbfd621d042ab1a2a685c3
XenoBoecker <xeno.boecker@web.de>
2025-07-02 02:11:38 +0200

    target, hitable and new crossbow bolt

---
+++ Assets/3_Interactables/Crossbow/Target.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Hitable))]
+public class Target : MonoBehaviour
+{
+    [SerializeField] private float slowMoveSpeed = 0.5f;
+    [SerializeField] private float fastMoveSpeed = 2f;
+
+
+    [SerializeField] private float hoverSpeed = 1f; // Speed of the up and down movement
+    [SerializeField] private float hoverHeight = 0.5f; // Height of the hover movement
+
+    [SerializeField] private Vector3 moveDirection = Vector3.right;
+
+    Hitable hitable;
+
+    float baseHeight;
+    bool isMovingFast = false;
+
+    bool movementDisabled;
+
+    private void Awake()
+    {
+        hitable = GetComponent<Hitable>();
+        hitable.OnHit += DisableMovement;
+    }
+
+    private void DisableMovement()
+    {
+        movementDisabled = true;
+    }
+
+    private void Start()
+    {
+        baseHeight = transform.position.y; // Store the initial height of the target
+    }
+
+    private void Update()
+    {
+        if(movementDisabled)
+        {
+            return;
+        }
+
+        float moveSpeed = isMovingFast ? fastMoveSpeed : slowMoveSpeed;
+
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+        HoverUpAndDown();
+    }
+
+    private void HoverUpAndDown()
+    {
+        float newY = baseHeight + Mathf.Sin(Time.time * hoverSpeed) * hoverHeight;
+        Vector3 newPosition = transform.position;
+        newPosition.y = newY;
+        transform.position = newPosition;
+    }
+
+    public void ToggleMovementSpeed()
+    {
+        isMovingFast = !isMovingFast; // Toggle between slow and fast movement
+        Debug.Log($"Target speed toggled to {(isMovingFast ? "fast" : "slow")}.");
+    }
+}
