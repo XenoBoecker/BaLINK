6e5c77a8796c04020ac1f64268fe3ee09bf2c715
BouncyPantaloons <71738150+BouncyPantaloons@users.noreply.github.com>
2025-07-03 01:46:58 +0200

    Fixed graph stuff to make dialogue integration better

--- Assets/20_Graph System/Runtime/Node Elements/Effects/PlayDialogueLine.cs
+++ Assets/20_Graph System/Runtime/Node Elements/Effects/PlayDialogueLine.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
 [NodeElement(NodeType.Effect)]
-[RequireComponent(typeof(DialogueRunner))]
 public class PlayDialogueLine : Effect
 {
     [SerializeField] private DialogueSequence _sequence;
@@ -11,6 +10,12 @@ public class PlayDialogueLine : Effect
 
     public override void TriggerEffect()
     {
+        GameObject foundObject = GameObject.FindGameObjectWithTag("Dialogue Runner");
+        if (foundObject == null || !foundObject.TryGetComponent(out _runner))
+        {
+            throw new System.Exception("There is no object tagged 'Dialogue Runner' in the scene");
+        }
+
         _runner = ReferencedObject.GetComponent<DialogueRunner>();
         _runner.PlayDialogueLine(_sequence, _lineIndex);
     }
