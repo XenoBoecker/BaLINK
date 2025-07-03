338032d535ce92a4b4d01f628f1b235884176bc3
BouncyPantaloons <71738150+BouncyPantaloons@users.noreply.github.com>
2025-07-03 02:54:30 +0200

    Nope D:

--- Assets/Editor/SelectionBaseEditor.cs
+++ Assets/Editor/SelectionBaseEditor.cs
@@ -5,9 +5,4 @@ using UnityEngine;
 [CustomEditor(typeof(SelectionBase))]
 public class SelectionBaseEditor : Editor
 {
-    private void OnEnable()
-    {
-        SelectionBase selectionBase = target as SelectionBase;
-        PrefabUtility.RevertPrefabInstance(selectionBase.gameObject, InteractionMode.AutomatedAction);
-    }
 }
