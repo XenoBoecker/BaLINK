e21abb77478c1785a72d8ce6def11368a8bf55a6
XenoBoecker <xeno.boecker@web.de>
2025-07-03 12:31:27 +0200

    crossbow overlay and adjusted weaponspawnpoint

--- Assets/3_Interactables/Crossbow/EquippedItem.cs
+++ Assets/3_Interactables/Crossbow/EquippedItem.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class EquippedItem : MonoBehaviour
@@ -7,12 +8,17 @@ public class EquippedItem : MonoBehaviour
     public bool HasBeenUsedThisGame => _usedCount != 0;
 
     protected bool _isEquipped; // Tracks if the item is currently equipped
+    public bool IsEquipped => _isEquipped;
+
+    public event Action<bool> OnEquippedChanged;
 
     public void SetIsEquipped(bool isEquipped)
     {
         if (_isEquipped == isEquipped) return; // No change in state
 
         _isEquipped = isEquipped;
+
+        OnEquippedChanged?.Invoke(_isEquipped);
     }
 
     public virtual void UseItem()
