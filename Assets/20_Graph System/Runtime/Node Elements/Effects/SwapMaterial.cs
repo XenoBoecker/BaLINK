616bd81004b53fe8b98e20d352ae78376b84f4b3
DominiqueJarrzzino <152449957+DominiqueJarrzzino@users.noreply.github.com>
2025-07-03 13:44:40 +0200

    Wire changing colors each blink + wires set up
    changed some scripts together with Kristoffer

---
+++ Assets/20_Graph System/Runtime/Node Elements/Effects/SwapMaterial.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+[NodeElement(NodeType.Effect)]
+[RequireComponent(typeof(WireGenerator))]
+public class SwapMaterial : Effect
+{
+    [SerializeField]
+    private Material _material;
+
+    public override string GetName()
+    {
+        return "Swap Material";
+    }
+
+    public override void TriggerEffect()
+    {
+        if (ReferencedObject.TryGetComponent(out WireGenerator generator))
+        {
+            generator.Material = _material;
+        }
+    }
+}
