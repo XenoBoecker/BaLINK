a4c9ce9688b9d0d405b65fafc9d86f77f839d0b0
BouncyPantaloons <71738150+BouncyPantaloons@users.noreply.github.com>
2025-07-05 18:27:03 +0200

    Audio system

---
+++ Assets/27_Audio System/Runtime/AudioSystemManager.cs
@@ -0,0 +1,36 @@
+using GameEvents;
+using System.Collections;
+using UnityEngine;
+
+public class AudioSystemManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        ObjectEvents.OnPlayAudio += PlayAudio;
+    }
+
+    private void OnDisable()
+    {
+        ObjectEvents.OnPlayAudio -= PlayAudio;
+    }
+
+    private void PlayAudio(AudioSystemClip clip, Vector3 position)
+    {
+        StartCoroutine(CreateSourceAndPlay(clip, position));
+    }
+
+    private IEnumerator CreateSourceAndPlay(AudioSystemClip clip, Vector3 position)
+    {
+        GameObject obj = Instantiate(clip.AudioSourcePrefab, transform);
+        obj.transform.position = position;
+        obj.transform.rotation = Quaternion.identity;
+
+        AudioSource source = obj.GetComponent<AudioSource>();
+        source.clip = clip.Clip;
+        source.Play();
+
+        yield return new WaitUntil(() => { return !source.isPlaying; });
+
+        Destroy(obj);
+    }
+}
