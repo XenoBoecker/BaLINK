41f00e2cfd210508f40b3c07b6eced9d74ea061f
BouncyPantaloons <71738150+BouncyPantaloons@users.noreply.github.com>
2025-07-09 04:05:05 +0200

    Cleaning Up Folder

--- Assets/50_Testing/SceneRoot.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class SceneRoot : MonoBehaviour
-{
-    [SerializeField] private TrainCarGenerator _trainCarGenerator;
-    public TrainCarGenerator TrainCarGenerator => _trainCarGenerator;
-}
